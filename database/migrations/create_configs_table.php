a7aa23689f83a911d6fef2649514ddd2d2439273
dimaserr5 <dimaserr5@gmail.com>
2022-10-18 12:03:31 +0700

    Add Configs || indexController || indexPage || Migrate || Model_Configs

---
+++ database/migrations/create_configs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configs', function (Blueprint $table) {
+            $table->string('name')->index();
+            $table->string('lang');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configs');
+    }
+}
